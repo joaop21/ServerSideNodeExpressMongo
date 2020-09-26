682d4b98aeb64b10ec36174e86ac843a990c758c
joaop21 <jpsilva9898@gmail.com>
2020-09-26 18:37:14 +0100

    Assignment 2

---
+++ week2/conFusionServer/models/leaders.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const leadersSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  abbr: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  designation: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  featured: {
+    type: Boolean,
+    default: true
+  }
+},
+  {
+
+    timestamps: true
+  });
+
+var Leaders = mongoose.model('Dish', leadersSchema);
+
+module.exports = Leaders;
--- week2/conFusionServer/routes/leaderRouter.js
+++ week2/conFusionServer/routes/leaderRouter.js
@@ -1,50 +1,96 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-  })
   .get((req, res, next) => {
-    res.end('Will send all the leaders to you!');
+      
+    Dishes.find({})
+      .then((dishes) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dishes);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
   })
   .post((req, res, next) => {
-    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+    
+    Dishes.create(req.body)
+      .then((dish) => {
+        console.log('Dish created', dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
   })
   .put((req, res, next) => {
+
     res.statusCode = 403;
-    res.end('PUT operation not supported on /leaders');
+    res.end('PUT operation not supported on /dishes');
+
   })
   .delete((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Deleting all the leaders!');
+
+    Dishes.remove({})
+      .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
   });
 
 leaderRouter.route('/:leaderId')
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-  })
   .get((req, res, next) => {
-    res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
+
+    Dishes.findById(req.params.dishId)
+      .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
   })
   .post((req, res, next) => {
+
     res.statusCode = 403;
-    res.end('POST operation not supported on /leaders/' + req.params.leaderId);
+    res.end('POST operation not supported on /dishes/' + req.params.dishId);
+
   })
   .put((req, res, next) => {
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name + ' with details: ' + req.body.description);
+
+    Dishes.findByIdAndUpdate(req.params.dishId, { 
+      $set: req.body
+    }, {new: true})
+      .then((dish) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(dish);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
   })
   .delete((req, res, next) => {
-    res.end('Deleting leader: ' + req.params.leaderId);
+
+    Dishes.findByIdAndRemove(req.params.dishId)
+      .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+      }, (err) => next(err))
+      .catch((err) => next(err));
+
   });
 
   module.exports = leaderRouter;
